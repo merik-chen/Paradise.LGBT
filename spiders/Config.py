bf1eefacd86a6170bfa98fb68333da81e0270036
Merik <merik@voicetube.com>
2017-07-09 16:15:42 +0800

    Upd

--- spiders/Config.py
+++ spiders/Config.py
@@ -9,12 +9,12 @@ app_env = 'ENV' in os.environ and str(os.environ['ENV']).lower() or ''
 
 if app_env == 'testing':
     app_cfg = {
-        'mongo': {'address': 'paradise.lgbt', 'port': 27017},
+        'mongo': {'address': 'paradise.mongo', 'port': 27017},
         'redis': {'address': 'paradise.lgbt', 'port': 6379},
     }
 elif app_env == 'spider':
     app_cfg = {
-        'mongo': {'address': '10.99.0.11', 'port': 27017},
+        'mongo': {'address': 'paradise.mongo', 'port': 27017},
         'redis': {'address': '10.99.0.11', 'port': 6379},
     }
 else:
